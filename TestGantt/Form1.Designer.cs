42301df138f50ace96ae2cce95654c41d324bda2
Kirill Berezin <enelar@exsul.net>
2015-11-23 12:13:54 +0300

    Testing C# features. Drawing raw rectangle

---
+++ TestGantt/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace TestGantt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gantt1 = new TestGantt.Gantt();
+            this.SuspendLayout();
+            // 
+            // gantt1
+            // 
+            this.gantt1.Location = new System.Drawing.Point(122, 85);
+            this.gantt1.Name = "gantt1";
+            this.gantt1.Size = new System.Drawing.Size(150, 150);
+            this.gantt1.TabIndex = 0;
+            this.gantt1.Load += new System.EventHandler(this.gantt1_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.gantt1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Gantt gantt1;
+
+
+    }
+}
+
---
+++ TestGantt/Point2d.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGantt
+{
+    class Point2d
+    {
+        public double x, y;
+    }
+}
